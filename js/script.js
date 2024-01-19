ce21e5cc3b7ddbb62aee3119b5d99e6f11f43e1b
KollihTheGuruh <mmike8449@gmail.com>
2024-01-19 22:27:17 +0300

    sushi

--- js/script.js
+++ js/script.js
@@ -2,9 +2,13 @@
 // import "aos/dist/aos.css";
 
 // init AOS animation
-AOS.init({
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+AOS.init ({
     duration: 1000,
-    offset: 100,
+    offset: 10;
 });
+    
 
 // the additional code you saw in the video will not be needed :)
